6e2a3f0902c05aebd88b92a376aec2b6df9e3fcb
Fajar-Islami <ahmadfajarislami@protonmail.com>
2022-07-02 22:33:49 +0700

    ## 5 - Database

---
+++ 5_database.sh
@@ -0,0 +1,7 @@
+docker exec -it redis_basic_2 bash
+
+redis-cli -h localhost
+
+select <database>
+select 1
+select 0
---
+++ common.sh
@@ -0,0 +1,4 @@
+# enter redis exec
+docker exec -it redis_basic_2 bash
+
+redis-cli -h localhost
